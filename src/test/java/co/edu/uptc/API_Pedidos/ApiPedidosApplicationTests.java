d079886ee10237e7481d78480d27e06a426e5f5a
EliasKmrgo <elias.camargo@uptc.edu.co>
2025-11-12 19:09:28 -0500

    Api pedidos

---
+++ src/test/java/co/edu/uptc/API_Pedidos/ApiPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.API_Pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
